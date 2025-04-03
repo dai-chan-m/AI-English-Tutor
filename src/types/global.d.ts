4b6d4b256b0cf4b6db0e1d1d9b3548a16a85dd39
daisukeMashiko <d_mashiko@qmu.jp>
2025-04-03 19:35:47 +0900

    型を自分で定義

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,20 @@
 export {};
 
+type SpeechRecognitionResultCallback = (event: SpeechRecognitionEvent) => void;
+
+interface SpeechRecognition {
+  lang: string;
+  continuous: boolean;
+  interimResults: boolean;
+  onresult: SpeechRecognitionResultCallback | null;
+  onerror: ((event: Event) => void) | null;
+  start: () => void;
+  stop: () => void;
+}
+
 declare global {
   interface Window {
-    SpeechRecognition: any;
-    webkitSpeechRecognition: any;
+    SpeechRecognition: new () => SpeechRecognition;
+    webkitSpeechRecognition: new () => SpeechRecognition;
   }
 }
